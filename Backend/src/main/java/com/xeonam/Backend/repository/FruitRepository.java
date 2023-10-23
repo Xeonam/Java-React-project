48b9f91d80ef4ca03fa8065a3ef38b7261cee696
Balázs Sidlóczki <sidbalazs18@gmail.com>
2023-10-23 21:03:00 +0200

    Add Fruit and Supplier repositories

---
+++ Backend/src/main/java/com/xeonam/Backend/repository/FruitRepository.java
@@ -0,0 +1,9 @@
+package com.xeonam.Backend.repository;
+
+import com.xeonam.Backend.model.Fruit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FruitRepository extends JpaRepository<Fruit, Long> {
+}
---
+++ Backend/src/main/java/com/xeonam/Backend/repository/SupplierRepository.java
@@ -0,0 +1,9 @@
+package com.xeonam.Backend.repository;
+
+import com.xeonam.Backend.model.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+}
