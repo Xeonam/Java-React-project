833f9a46bf76e8d8dd0728d763431187b32e2c4d
Balázs Sidlóczki <sidbalazs18@gmail.com>
2023-10-23 21:17:48 +0200

    Add FruitDto and SupplierDto

---
+++ Backend/src/main/java/com/xeonam/Backend/dto/FruitDto.java
@@ -0,0 +1,18 @@
+package com.xeonam.Backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FruitDto {
+    private Long id;
+    private String name;
+    private double price;
+    private SupplierDto supplier;
+
+}
