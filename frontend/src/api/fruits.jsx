115196da7aac660434cd567ad78af2c4e2728669
Balázs Sidlóczki <sidbalazs18@gmail.com>
2023-11-15 11:57:03 +0100

    Implement the process of adding fruits via formik

--- frontend/src/api/fruits.jsx
+++ frontend/src/api/fruits.jsx
@@ -1,3 +1,5 @@
+import { json } from "react-router-dom";
+
 export async function fetchFruits() {
     const response = await fetch('http://localhost:8080/fruits')
     return response.json()
@@ -7,6 +9,17 @@ export async function deleteFruit(id) {
     const response = await fetch(`http://localhost:8080/delete-fruit/${id}`,{
         method: "DELETE"
     });
-    console.log(response)
     return response
+}
+
+export async function createFruit(newFruit){
+    const response = await fetch('http://localhost:8080/add-fruit',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newFruit)
+    });
+    return response
+    
 }
--- frontend/src/components/AddFruit.jsx
+++ frontend/src/components/AddFruit.jsx
@@ -1,48 +1,126 @@
 import React from "react";
-import { Formik, Field, Form } from "formik";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { fetchSuppliers } from "../api/suppliers";
-
-
+import { createFruit } from "../api/fruits";
+import { toast, Toaster } from "sonner";
+import { Card } from "flowbite-react";
 
 function AddFruit() {
-  const queryClient = useQueryClient();
-
-  const {isPending, isError, data } = useQuery({
+  const { isPending, isError, data, error} = useQuery({
     queryKey: ["suppliersData"],
     queryFn: fetchSuppliers,
   });
+
+  const createFruitMutation = useMutation({
+    mutationFn: createFruit,
+  });
+
+  const handleAddFruit = (fruit) => {
+    createFruitMutation.mutate({
+      ...fruit,
+    });
+  };
 
   if (isPending) return "Loading...";
   if (isError) return "An error has occurred: " + error.message;
 
   return (
-    <div className="max-w-[1000px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-      <Formik>
-        <Form>
-          <div>
-            <Field name="name" type="text" placeholder="Fruit"></Field>
-          </div>
+    <div className="h-screen w-full grid place-items-center">
+      <Formik
+        initialValues={{
+          name: "",
+          price: "",
+          supplier: "",
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string().required("Name is required"),
+          price: Yup.number("Number")
+            .typeError("You must specify a number")
+            .required("Price is required")
+            .positive("Must be a positive number"),
+          supplier: Yup.number()
+            .oneOf(
+              data.map((supplier) => supplier.id),
+              "Please select a valid supplier"
+            )
+            .required("Supplier is required"),
+        })}
+        onSubmit={(values, actions) => {
+          const modifiedValues = {
+            ...values,
+            supplier: {
+              id: parseInt(values.supplier),
+            },
+          };
 
-          <div>
-            <Field name="price" type="text" placeholder="Price"></Field>
-          </div>
+          handleAddFruit(modifiedValues);
+          actions.resetForm({
+            values: {
+              name: "",
+              price: "",
+              supplier: "",
+            },
+          });
+          
+          toast.success("Added");
+        }}
+      >
+        <Card className="">
+          <Form className="grid space-y-3">
+            <Field
+              name="name"
+              type="text"
+              placeholder="Fruit"
+              className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+            ></Field>
+            <ErrorMessage
+              component="label"
+              name="name"
+              className="text-red-600"
+            />
 
-          <Field as="select" name="supplier">
-            <option value="">Supplier</option>
-            {data.map((supplier) => (
-              <option key={supplier.id} value={supplier.id}>
-                {supplier.id}
-              </option>
-            ))}
-          </Field>
-          <div>
-          <button type="submit" >
+            <Field
+              name="price"
+              type="text"
+              placeholder="Price"
+              className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+            ></Field>
+            <ErrorMessage
+              component="label"
+              name="price"
+              className="text-red-600"
+            />
+
+            <Field
+              as="select"
+              name="supplier"
+              className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+            >
+              <option value="">Choose a supplier</option>
+              {data.map((supplier) => (
+                <option key={supplier.id} value={supplier.id}>
+                  {supplier.name}
+                </option>
+              ))}
+            </Field>
+            <ErrorMessage
+              component="label"
+              name="supplier"
+              className="text-red-600"
+            />
+            <div className="grid justify-center">
+              <button
+                className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                type="submit"
+              >
                 Add Fruit
-            </button>
-          </div>
-            
-        </Form>
+              </button>
+            </div>
+            <Toaster richColors />
+          </Form>
+        </Card>
       </Formik>
     </div>
   );
